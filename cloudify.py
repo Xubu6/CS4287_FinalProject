aa4716187848b93a9eda1dbe906e3df90c49d3ab
Xubu6 <a.x.alxu@gmail.com>
2021-12-15 14:57:23 -0600

    final submission

--- cloudify.py
+++ cloudify.py
@@ -26,7 +26,7 @@ class CloudifyARData:
             self.mysql_db = mysql.connector.connect(host='localhost',
                                         database='ar_project',
                                         user='admin',
-                                        password='password',
+                                        password='<password>',
                                         use_pure=True)
             if self.mysql_db.is_connected():
                 self.debug(
